f18c01aa95b006a771ffd5fae7eb485326ea6c07
Thant Nay Lin <thantnaylin1301@gmail.com>
2021-05-28 18:22:39 +0800

    Added header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+import Header from "./components/layouts/Header";
+import Footer from "./components/layouts/Footer";
+
 const App = () => {
   return (
     <>
-      <h1>Welcome to Market-place</h1>
+      <Header />
+      <Container>
+        <main className="py-3">
+          <h1>Welcome to Market-place</h1>
+        </main>
+      </Container>
+      <Footer />
     </>
   );
 };
