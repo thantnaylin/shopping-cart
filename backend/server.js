fc5a61f4562ceb52acf91fd9e915edeaeaa06f91
Thant Nay Lin <thantnaylin1301@gmail.com>
2021-05-30 14:17:35 +0800

    Added db connection

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -16,6 +24,13 @@ app.get("/api/products/:id", (req, res) => {
   return res.json(product);
 });
 
-app.listen(5000, console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
 
 export default app;
